c9e27843428bd5a67056cfa3de56e56405a6c402
ShanjinurIslam <spondoncsebuet@gmail.com>
2019-05-07 20:20:43 +0600

    adding background image in the auth page

--- snatok/lib/views/auth.dart
+++ snatok/lib/views/auth.dart
@@ -18,86 +18,90 @@ class _AuthPageState extends State<AuthPageView> {
         appBar: AppBar(
           title: Text('Login'),
         ),
-        body: Center(
-          child: SingleChildScrollView(
-              child: Column(
-            children: <Widget>[
-              SizedBox(
-                height: 10,
-              ),
-              Center(
-                child: Container(
-                  margin: EdgeInsets.all(10),
-                  child: Text(
-                    'স্নাতক',
-                    style: TextStyle(fontSize: 40),
+        body: Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(colorFilter: ColorFilter.mode(Colors.white.withOpacity(0.5), BlendMode.dstATop),
+                    fit: BoxFit.cover, image: AssetImage('assets/2.jpg'))),
+            child: Center(
+              child: SingleChildScrollView(
+                  child: Column(
+                children: <Widget>[
+                  SizedBox(
+                    height: 10,
                   ),
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.all(10),
-                child: FractionallySizedBox(
-                  child: TextField(
-                    style: TextStyle(color: Colors.black),
-                    decoration: InputDecoration(
-                        enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(12)),
-                        labelText: 'Username',
-                        filled: true,
-                        fillColor: Colors.white,
-                        labelStyle: TextStyle(color: Colors.grey)),
-                    onChanged: (String value) {},
+                  Center(
+                    child: Container(
+                      margin: EdgeInsets.all(10),
+                      child: Text(
+                        'স্নাতক',
+                        style: TextStyle(fontSize: 40),
+                      ),
+                    ),
                   ),
-                  widthFactor: 0.85,
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.all(10),
-                child: FractionallySizedBox(
-                  child: TextField(
-                    style: TextStyle(color: Colors.black),
-                    decoration: InputDecoration(
-                        enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(12)),
-                        labelText: 'Password',
-                        filled: true,
-                        fillColor: Colors.white,
-                        labelStyle: TextStyle(color: Colors.grey)),
-                    onChanged: (String value) {},
+                  Container(
+                    margin: EdgeInsets.all(10),
+                    child: FractionallySizedBox(
+                      child: TextField(
+                        style: TextStyle(color: Colors.black),
+                        decoration: InputDecoration(
+                            enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(12)),
+                            labelText: 'Username',
+                            filled: true,
+                            fillColor: Colors.white,
+                            labelStyle: TextStyle(color: Colors.grey)),
+                        onChanged: (String value) {},
+                      ),
+                      widthFactor: 0.85,
+                    ),
                   ),
-                  widthFactor: 0.85,
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.fromLTRB(25, 0, 25, 0),
-                child: SwitchListTile(
-                  value: acceptTerms,
-                  onChanged: (bool value) {
-                    setState(() {
-                      acceptTerms = value;
-                    });
-                  },
-                  title: Text('Remember me?'),
-                ),
-              ),
-              Container(
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Colors.green,
-                    border: Border.all(color: Colors.white)),
-                child: FlatButton(
-                  child: Text(
-                    'Log In',
-                    style: TextStyle(color: Colors.white),
+                  Container(
+                    margin: EdgeInsets.all(10),
+                    child: FractionallySizedBox(
+                      child: TextField(
+                        style: TextStyle(color: Colors.black),
+                        decoration: InputDecoration(
+                            enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(12)),
+                            labelText: 'Password',
+                            filled: true,
+                            fillColor: Colors.white,
+                            labelStyle: TextStyle(color: Colors.grey)),
+                        onChanged: (String value) {},
+                      ),
+                      widthFactor: 0.85,
+                    ),
                   ),
-                  onPressed: () {
-                    Navigator.pushReplacementNamed(context, '/home');
-                  },
-                  color: Colors.green,
-                ),
-              ),
-            ],
-          )),
-        ));
+                  Container(
+                    margin: EdgeInsets.fromLTRB(25, 0, 25, 0),
+                    child: SwitchListTile(
+                      value: acceptTerms,
+                      onChanged: (bool value) {
+                        setState(() {
+                          acceptTerms = value;
+                        });
+                      },
+                      title: Text('Remember me?'),
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.green,
+                        border: Border.all(color: Colors.white)),
+                    child: FlatButton(
+                      child: Text(
+                        'Log In',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      onPressed: () {
+                        Navigator.pushReplacementNamed(context, '/home');
+                      },
+                      color: Colors.green,
+                    ),
+                  ),
+                ],
+              )),
+            )));
   }
 }
